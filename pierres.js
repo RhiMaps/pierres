8dc4dabbad2a35162a136d826afd130f37581c5b
Richard Hitier <richard.hitier@co-libri.org>
2014-06-27 10:13:19 +0200

    project init

---
+++ pierres.js
@@ -0,0 +1,93 @@
+var z=9;
+var myLL= L.latLng(43.59, 1.45);
+
+function init(){
+
+
+  // create a map in the "map" div, set the view to a given place and zoom
+    var map = L.map('map', {
+      center:myLL,
+      zoom:z});
+
+  // add an OpenStreetMap tile layer
+  var osmLayer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+      attribution: 'OpenStreetMap'
+      });
+  osmLayer.addTo(map);
+  var mapqLayer = L.tileLayer('http://{s}.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png', {
+      attribution: 'MapQuest OpenStreetMap',
+      subdomains: ['otile1','otile2','otile3','otile4']
+      });
+  mapqLayer.addTo(map);
+
+
+
+  var mnhOptions = {
+      radius: 8,
+      fillColor: "red",
+      color: "black",
+      weight: 1,
+      opacity: 1,
+      fillOpacity: 0.8
+  };
+  var dlmOptions = {
+      radius: 8,
+      fillColor: "yellow",
+      color: "#000",
+      weight: 1,
+      opacity: 1,
+      fillOpacity: 0.8
+  };
+
+  var dlmLayer = L.geoJson(  archeological, {
+      pointToLayer: function (feature, latlng) {
+          return L.circleMarker(latlng, dlmOptions);
+      },
+      style: archeoColor,
+    onEachFeature: function (feature, layer) {
+        layer.bindPopup(feature.properties.site_type);
+    }
+  });
+  dlmLayer.addTo(map);
+
+  function archeoColor(feature){
+
+      var archeoType=feature.properties.site_type;
+      var archeoColor;
+      switch( archeoType){
+          case 'fortification':
+              archeoColor='red'; break;
+          case 'bigstone':
+              archeoColor='blue'; break;
+          case 'dolmen':
+              archeoColor='green'; break;
+          case 'megalith':
+              archeoColor='yellow'; break;
+          default:
+              archeoColor='white'; 
+      }
+
+      return {fillColor: archeoColor};
+
+  };
+
+
+
+
+  var baseLayers = {
+    "MapBox": mapqLayer,
+    "OSM": osmLayer
+  };
+
+  var overlays = {
+    "pierres": dlmLayer,
+  };
+
+  L.control.layers(baseLayers, overlays).setPosition('bottomleft').addTo(map);
+
+
+}
+
+
+init();
+
