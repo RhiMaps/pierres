77df80fd306b093dd79c43f4170c79e43e844faa
Richard Hitier <richard.hitier@co-libri.org>
2014-07-09 11:19:29 +0200

    Remove deprecated icon and popup callbacks

--- pierres.js
+++ pierres.js
@@ -1,8 +1,7 @@
-var z = 11;
-var myLL = L.latLng(43.3317, 2.5808);
-
-
 function init() {
+
+    var z = 11;
+    var myLL = L.latLng(43.3317, 2.5808);
 
 
     // create a map in the "map" div, set the view to a given place and zoom
@@ -11,6 +10,9 @@ function init() {
         zoom: z
     });
 
+    // 
+    // Map Layers
+    //
 
     var esriLayer = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
         attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
@@ -34,8 +36,6 @@ function init() {
         maxZoom: 20
     });
 
-
-    // add an OpenStreetMap tile layer
     var osmLayer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
         attribution: 'OpenStreetMap'
     });
@@ -51,62 +51,6 @@ function init() {
     // POIS Overlays 
     //
 
-
-
-    function archeoMarker(feature, latlng) {
-        var imageUrl;
-        var archeoType = feature.properties.site_type;
-        switch (archeoType) {
-            case 'dolmen':
-                imageUrl = 'images/dolmen.png';
-                break;
-            case "megalih":
-            case "meglith":
-            case 'megalith':
-                imageUrl = 'images/menhir.png';
-                break;
-            case 'fortification':
-                imageUrl = 'images/fortifications.png';
-                break;
-            case 'tumulus':
-                imageUrl = 'images/tumulus.png';
-                break;
-            default:
-                imageUrl = 'images/ruine.png';
-                break;
-                /*
-            "site_type": "petroglyph",
-            "site_type": "quarry"
-            "site_type": "villa",
-            */
-        }
-        var myIcon = L.icon({
-            iconUrl: imageUrl,
-            iconSize: [40, 40],
-            iconAnchor: [20, 20],
-            popupAnchor: [0, -20]
-        });
-        var marker = L.marker(latlng, {
-            icon: myIcon
-        });
-        return marker;
-    }
-
-    function archeoPopup(feature, layer) {
-        var popupTxt = '';
-        if (undefined == feature.properties.site_type)
-            popupTxt += '';
-        else
-            popupTxt += feature.properties.site_type;
-
-        if (undefined == feature.properties.name)
-            popupTxt += '';
-        else
-            popupTxt += '</br>' + feature.properties.name;
-
-        layer.bindPopup(popupTxt);
-    }
-
     function overpassPopup(data, marker) {
         var divPopup = "";
         if (data.tags.name) divPopup += "<b>name: </b>" + data.tags.name + "<br/>";
@@ -114,6 +58,37 @@ function init() {
         if (data.tags.megalith_type) divPopup += "<b>megalith_type: </b>" + data.tags.megalith_type + "<br/>";
         if (data.tags.source) divPopup += "<b>source: </b>" + toHref(data.tags.source) + "<br/>";
         return divPopup;
+    }
+
+    var loader = L.DomUtil.get('loader');
+
+    function jsonLoading(e) {
+        console.log(e.target._map._zoom);
+        loader.style.display = 'block';
+    }
+
+    function jsonLoaded(e) {
+        console.log(e.target._map._zoom);
+        loader.style.display = 'none';
+    }
+
+    L.layerJSON({
+        url: 'http://overpass-api.de/api/interpreter?data=[out:json];node({lat1},{lon1},{lat2},{lon2})[historic=archaeological_site];out;',
+        propertyItems: 'elements',
+        propertyTitle: 'tags.name',
+        propertyLoc: ['lat', 'lon'],
+        buildIcon: overpassIcon,
+        buildPopup: overpassPopup,
+        minZoom: 9,
+    }).on('dataloading', jsonLoading)
+        .on('dataloaded', jsonLoaded)
+        .addTo(map);
+
+    function toHref(ref) {
+        var href = ref;
+        if (ref.indexOf("http") > -1) href = "<a href=\"" + ref + "\">" + ref + "</a>";
+        console.log("href: " + href);
+        return href;
     }
 
     function overpassIcon(data, title) {
@@ -154,38 +129,9 @@ function init() {
     }
 
 
-    var loader = L.DomUtil.get('loader');
-
-    function jsonLoading(e) {
-        console.log(e.target._map._zoom);
-        loader.style.display = 'block';
-    }
-
-    function jsonLoaded(e) {
-        console.log(e.target._map._zoom);
-        loader.style.display = 'none';
-    }
-
-    L.layerJSON({
-        url: 'http://overpass-api.de/api/interpreter?data=[out:json];node({lat1},{lon1},{lat2},{lon2})[historic=archaeological_site];out;',
-        propertyItems: 'elements',
-        propertyTitle: 'tags.name',
-        propertyLoc: ['lat', 'lon'],
-        buildIcon: overpassIcon,
-        buildPopup: overpassPopup,
-        minZoom: 9,
-    })
-        .on('dataloading', jsonLoading)
-        .on('dataloaded', jsonLoaded)
-        .addTo(map);
-
-    function toHref(ref) {
-        var href = ref;
-        if (ref.indexOf("http") > -1) href = "<a href=\"" + ref + "\">" + ref + "</a>";
-        console.log("href: " + href);
-        return href;
-    }
-
+    //
+    // Map Controls and Layers
+    //
 
     var baseLayers = {
         "Satellite": esriLayer,
@@ -194,6 +140,7 @@ function init() {
         "Hybrid": hybridDay,
         "Basic": stamenLayer,
     };
+
 
     // layers switcher
     L.control.layers(baseLayers).setPosition('topright').addTo(map);
