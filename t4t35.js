43860013ddcc30566e80e2ccb8e08691c8160bc9
Richard Hitier <richard.hitier@co-libri.org>
2014-07-11 23:05:09 +0200

    Show first t4t35 csv as a layer

---
+++ t4t35.js
@@ -0,0 +1,40 @@
+var t4t35Layer = L.geoCsv(null, {
+    firstLineTitles: true,
+    fieldSeparator: ';',
+    onEachFeature: function (feature, layer) {
+        console.log( feature);
+        var popupContent= '<b>'+feature.properties['nomsite']+'</b>'
+        var popup = L.popup().setContent(popupContent);
+        layer.bindPopup(popup);
+    },
+//    pointToLayer: function(feature, latlng) {
+//          var myIcon = L.icon({
+//              iconUrl: 'images/chevre.png',
+//              iconSize: [40, 40],
+//              iconAnchor: [20, 20],
+//              popupAnchor: [0, -20]});
+//          var marker = L.marker(latlng, {icon:myIcon} );
+//          return  marker;
+//      },
+});
+
+
+$.ajax ({
+    type:'GET',
+    dataType:'text',
+    url:'datas/T4T35-tous-82.csv',
+   error: function() {
+     alert('Chargement impossible');
+   },
+    success: function(csv) {
+      //var cluster = new L.MarkerClusterGroup();
+        t4t35Layer.addData(csv);
+        //cluster.addLayer(bankias);
+        //map.addLayer(cluster);
+        map.addLayer(t4t35Layer);
+        map.fitBounds(t4t35Layer.getBounds());
+    },
+   complete: function() {
+      //$('#cargando').delay(500).fadeOut('slow');
+   }
+});
