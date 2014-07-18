b7175c27b51b3a90da812e2b9bd98d1a54d1691a
Richard Hitier <richard.hitier@co-libri.org>
2014-07-18 07:50:28 +0200

    Split long string

--- pierres.js
+++ pierres.js
@@ -15,7 +15,9 @@ function init() {
     //
 
     var esriLayer = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
-        attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+        attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, '+
+                     'USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, '+
+                     'and the GIS User Community'
     }).addTo(map);
 
     var stamenLayer = L.tileLayer('http://{s}.tile.stamen.com/toner-lite/{z}/{x}/{y}.png', {
